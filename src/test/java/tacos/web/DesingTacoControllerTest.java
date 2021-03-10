7044b8dbdcadc060f364544e1d74c0eb50fe978a
Aleksandr Uvarov <sanyauvarov@mail.ru>
2021-03-10 22:32:51 +0300

    283

--- src/test/java/tacos/web/DesingTacoControllerTest.java
+++ src/test/java/tacos/web/DesingTacoControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import tacos.Ingredient;
 import tacos.Taco;
 import tacos.data.IngredientRepository;
@@ -17,6 +18,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
@@ -35,10 +37,10 @@ class DesingTacoControllerTest {
                 testTaco(15L), testTaco(16L)};
         Flux<Taco> tacoFlux = Flux.just(tacos);
 
+        IngredientRepository ingredientRepo = Mockito.mock(IngredientRepository.class);
+
         TacoRepository tacoRepo = Mockito.mock(TacoRepository.class);
         when(tacoRepo.findAll()).thenReturn(tacoFlux);
-
-        IngredientRepository ingredientRepo = Mockito.mock(IngredientRepository.class);
 
         WebTestClient testClient = WebTestClient.bindToController(
                 new DesingTacoController(ingredientRepo, tacoRepo)).build();
@@ -49,6 +51,31 @@ class DesingTacoControllerTest {
                 .expectStatus().isOk()
                 .expectBodyList(Taco.class)
                    .contains(Arrays.copyOf(tacos, 12));
+    }
+
+    @Test
+    public void shouldSaveTaco() {
+        Mono<Taco> unsavedTacoMono = Mono.just(testTaco(null));
+        Taco savedTaco = testTaco(null);
+        savedTaco.setId(1L);
+        Mono<Taco> savedTacoMono = Mono.just(savedTaco);
+
+        IngredientRepository ingredientRepo = Mockito.mock(IngredientRepository.class);
+
+        TacoRepository tacoRepo = Mockito.mock(TacoRepository.class);
+        when(tacoRepo.save(any())).thenReturn(savedTacoMono);
+
+        WebTestClient testClient = WebTestClient.bindToController(
+                new DesingTacoController(ingredientRepo, tacoRepo)).build();
+
+        testClient.post()
+                    .uri("/design")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(unsavedTacoMono, Taco.class)
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(Taco.class)
+                    .isEqualTo(savedTaco);
     }
 
     private Taco testTaco(Long number) {
