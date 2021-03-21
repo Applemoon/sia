109165718dd9d3c3540fecde365452a884aecb0a
Aleksandr Uvarov <sanyauvarov@mail.ru>
2021-03-21 10:42:00 +0300

    312

--- src/main/java/tacos/data/TacoRepository.java
+++ src/main/java/tacos/data/TacoRepository.java
@@ -1,8 +1,9 @@
 package tacos.data;
 
+import java.util.UUID;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import tacos.Taco;
 
-public interface TacoRepository extends ReactiveCrudRepository<Taco, Long> {
+public interface TacoRepository extends ReactiveCrudRepository<Taco, UUID> {
 
 }
